a7d0b36b012f44b951270999669203254b25987b
ssd <ssd_chan@yahoo.co.jp>
2017-03-27 04:05:14 +0900

    database init ok

---
+++ app/src/main/java/com/example/ssd/shimaneuniversitybrowser/DBAdapter.java
@@ -0,0 +1,229 @@
+package com.example.ssd.shimaneuniversitybrowser;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Databaseに関連するクラス
+ * DBAdapter
+ */
+public class DBAdapter {
+
+    private final static String DB_NAME = "shimane_university.db";      // DBファイル名
+    private final static int DB_VERSION = 1;                // DBのバージョン
+
+    private SQLiteDatabase db = null;           // SQLiteDatabase
+    private DBHelper dbHelper = null;           // DBHelper
+    protected Context context;                  // Context
+
+    // コンストラクタ
+    public DBAdapter(Context context) {
+        this.context = context;
+        dbHelper = new DBHelper(this.context);
+    }
+
+    /**
+     * DBの読み書き
+     * openDB()
+     *
+     * @return this 自身のオブジェクト
+     */
+    public DBAdapter openDB() {
+        db = dbHelper.getWritableDatabase();        // DBの読み書き
+        return this;
+    }
+
+    /**
+     * DBの読み込み 今回は未使用
+     * readDB()
+     *
+     * @return this 自身のオブジェクト
+     */
+    public DBAdapter readDB() {
+        db = dbHelper.getReadableDatabase();        // DBの読み込み
+        return this;
+    }
+
+    /**
+     * DBを閉じる
+     * closeDB()
+     */
+    public void closeDB() {
+        db.close();     // DBを閉じる
+        db = null;
+    }
+
+    /**
+     * DBのレコードへ登録
+     * saveDB()
+     *
+     * @param table テーブル名
+     * @param column 列名
+     * @param value 値
+     */
+    public void saveDB( String table, String[] column, String[] value) {
+
+        db.beginTransaction();          // トランザクション開始
+
+        try {
+            ContentValues values = new ContentValues();     // ContentValuesでデータを設定していく
+            for(int i = 0; i < column.length; i++) {
+                values.put( column[i], value[i]);
+            }
+
+            // insertメソッド データ登録
+            // 第1引数：DBのテーブル名
+            // 第2引数：更新する条件式
+            // 第3引数：ContentValues
+            db.insert( table, null, values);      // レコードへ登録
+
+            db.setTransactionSuccessful();      // トランザクションへコミット
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();                // トランザクションの終了
+        }
+    }
+
+    /**
+     * DBのデータを取得
+     * getDB()
+     *
+     * @param table String テーブル名
+     * @param columns String[] 取得するカラム名 nullの場合は全カラムを取得
+     * @return DBのデータ
+     */
+    public Cursor getDB(String table, String[] columns) {
+
+        // queryメソッド DBのデータを取得
+        // 第1引数：DBのテーブル名
+        // 第2引数：取得するカラム名
+        // 第3引数：選択条件(WHERE句)
+        // 第4引数：第3引数のWHERE句において?を使用した場合に使用
+        // 第5引数：集計条件(GROUP BY句)
+        // 第6引数：選択条件(HAVING句)
+        // 第7引数：ソート条件(ORDER BY句)
+        return db.query( table, columns, null, null, null, null, null);
+    }
+
+    /**
+     * DBの検索したデータを取得
+     * searchDB()
+     *
+     * @param table String 取得するテーブル名
+     * @param columns String[] 取得するカラム名 nullの場合は全カラムを取得
+     * @param column  String 選択条件に使うカラム名
+     * @param name    String[]
+     * @return DBの検索したデータ
+     */
+    public Cursor searchDB(String table, String[] columns, String column, String[] name) {
+        return db.query( table, columns, column + " like ?", name, null, null, null);
+    }
+
+    /**
+     * DBのレコードを全削除
+     * allDelete()
+     *
+     * @param table 削除するテーブル名
+     */
+    public void allDelete(String table) {
+
+        db.beginTransaction();                      // トランザクション開始
+        try {
+            // deleteメソッド DBのレコードを削除
+            // 第1引数：テーブル名
+            // 第2引数：削除する条件式 nullの場合は全レコードを削除
+            // 第3引数：第2引数で?を使用した場合に使用
+            db.delete( table, null, null);        // DBのレコードを全削除
+            db.setTransactionSuccessful();          // トランザクションへコミット
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();                    // トランザクションの終了
+        }
+    }
+
+    /**
+     * DBのレコードの単一削除
+     * selectDelete()
+     *
+     * @param table String 削除するテーブル名
+     * @param position String
+     */
+    public void selectDelete( String table, String position) {
+
+        db.beginTransaction();                      // トランザクション開始
+        try {
+            db.delete( table, "_id=?", new String[]{position});
+            db.setTransactionSuccessful();          // トランザクションへコミット
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();                    // トランザクションの終了
+        }
+    }
+
+    /**
+     * データベースの生成やアップグレードを管理するSQLiteOpenHelperを継承したクラス
+     * DBHelper
+     */
+    private static class DBHelper extends SQLiteOpenHelper {
+
+        // コンストラクタ
+        public DBHelper(Context context) {
+            //第1引数：コンテキスト
+            //第2引数：DBファイル名
+            //第3引数：factory nullでよい
+            //第4引数：DBのバージョン
+            super( context, DB_NAME, null, DB_VERSION);
+        }
+
+        /**
+         * DB生成時に呼ばれる
+         * onCreate()
+         *
+         * @param db SQLiteDatabase
+         */
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            //テーブルを作成する ※スペースに気を付ける
+            db.execSQL(
+                    "CREATE TABLE ClassroomDivide ("
+                            + "_id INTEGER PRIMARY KEY NOT NULL, "
+                            + "building_id INTEGER NOT NULL, "
+                            + "place TEXT NOT NULL, "
+                            + "weekday INTEGER NOT NULL, "
+                            + "time INTEGER NOT NULL, "
+                            + "cell_text TEXT, "
+                            + "cell_color TEXT, "
+                            + "classname TEXT, "
+                            + "person TEXT, "
+                            + "department TEXT, "
+                            + "class_code TEXT);" );
+            db.execSQL(
+                    "CREATE TABLE Building ("
+                            + "_id INTEGER PRIMARY KEY NOT NULL, "
+                            + "building_name TEXT);" );
+        }
+
+        /**
+         * DBアップグレード(バージョンアップ)時に呼ばれる
+         *
+         * @param db         SQLiteDatabase
+         * @param oldVersion int 古いバージョン
+         * @param newVersion int 新しいバージョン
+         */
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // DBからテーブル削除
+            db.execSQL("DROP TABLE IF EXISTS ClassroomDivide");
+            db.execSQL("DROP TABLE IF EXISTS Building");
+            // テーブル生成
+            onCreate(db);
+        }
+    }
+}
--- app/src/main/java/com/example/ssd/shimaneuniversitybrowser/MainActivity.java
+++ app/src/main/java/com/example/ssd/shimaneuniversitybrowser/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.ssd.shimaneuniversitybrowser;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,5 +51,176 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //データを取得し、データベースに登録を行う
+        HTMLPageReceiver receiver = new HTMLPageReceiver(this);
+        //非同期処理を行う
+        receiver.execute("");
+
+    }
+
+    private class HTMLPageReceiver extends AsyncTask<String, String, Void> {
+
+        private Activity parentActivity;
+        private ProgressDialog m_ProgressDialog;
+        private Elements[] tableData = null;
+        private DBAdapter dbAdapter;
+
+        public HTMLPageReceiver(Activity activity){
+            parentActivity = activity;
+            dbAdapter = new DBAdapter(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+            // プログレスダイアログの生成
+            this.m_ProgressDialog = new ProgressDialog(parentActivity);
+            // プログレスダイアログの設定
+            this.m_ProgressDialog.setMessage(getResources().getString(R.string.message_now_loading));  // メッセージをセット
+            // プログレスダイアログの表示
+            this.m_ProgressDialog.show();
+
+        }
+
+        @Override
+        public Void doInBackground(String... params) {
+
+            if(tableData == null) { //データが取得されていない
+
+                //読み込みページ一覧を取得
+                String url = "http://www.shimane-u.ac.jp/education/school_info/class_data/class_data01.html";
+                String select = ".body li a"; //select tag
+
+                //DBを開く
+                dbAdapter.openDB();
+                dbAdapter.allDelete("Building");
+
+                //一覧のそれぞれの情報を取得
+                try {
+
+                    Document doc = Jsoup.connect(url).get();
+                    Elements urlList = doc.select(select);
+                    tableData = new Elements[urlList.size()];
+                    int i = 0;
+                    for (Element element : urlList) {
+                        try {
+                            String name = element.text().replace("教室配当表_", "").replaceAll("_", " ");
+                            String URL = element.attr("abs:href");
+
+                            //DBへの登録
+                            String[] column = {"_id", "building_name"};
+                            String[] value = { String.valueOf(i), name};
+                            dbAdapter.saveDB("Building", column, value);
+
+                            tableData[i] = Jsoup.connect(URL).get().select(".body tr");
+                        } catch (Exception e) {
+                            tableData[i] = null;
+                        } finally {
+                            i++;
+                        }
+                    }
+
+                } catch (Exception e) {
+                    tableData = null;
+                }finally {
+                    dbAdapter.closeDB();
+                }
+            }
+
+            return null;
+        }
+
+        public void onPostExecute(Void none) {
+
+            // プログレスダイアログを閉じる
+            if (m_ProgressDialog != null && m_ProgressDialog.isShowing()) {
+                m_ProgressDialog.dismiss();
+            }
+
+            if (tableData == null) {
+                new AlertDialog.Builder(parentActivity)
+                        .setTitle(R.string.error_title_data_response)
+                        .setMessage(R.string.error_message_response_refuse)
+                        .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //okボタンが押された時の処理
+                                parentActivity.finish();
+                            }
+                        })
+                        .show();
+            } else {
+
+                try {
+
+                    //DBを開く
+                    dbAdapter.openDB();
+                    dbAdapter.allDelete("ClassroomDivide");
+
+                    for(int d = 0; d < 5; d++) { //月から金のループ
+                        int point = d * 5 + 2; //trタグの位置
+                        for (int i = 0; i < 5; i++) { //1から5コマのループ
+
+                            int building_id = 0;
+                            for (Elements elements : tableData) { //建物のループ
+
+                                if (elements != null) {
+                                    String[] places = new String[elements.get(2).select("td").size() - 2];
+                                    int k = 0, l = 0;
+                                    for (int h = 1; h < elements.get(0).select("td").size(); h++) {
+                                        Element td = elements.get(0).select("td").get(h);
+                                        for (int j = 0; j < (td.attr("colspan").equals("") ? 1 : Integer.parseInt(td.attr("colspan"))); j++) {
+                                            if (td.attr("rowspan").equals(""))
+                                                places[l++] = td.text() + "_" + elements.get(1).select("td").get(k++).text();
+                                            else places[l++] = td.text();
+                                        }
+                                    }
+                                    for (int j = 0; j < places.length; j++) {
+                                        places[j] = places[j].replaceAll("_", " "); //_を空白にする
+                                        places[j] = places[j].replaceAll("　", " "); //全角空白を半角空白にする
+                                        places[j] = StringUtil.fullWidthNumberToHalfWidthNumber(places[j]); //全角数字を半角数字に変換する
+                                    }
+
+                                    Elements tdData = elements.get(point + i).select("td");
+                                    for (int pn = 0; pn < places.length; pn++) { //場所のループ
+                                        String text = tdData.get(i == 0 ? pn + 2 : pn + 1).text(); //一番最初は曜日名が入るので一つずらす
+
+                                        String color = "#000000";
+                                        if (text.equals(String.valueOf('\u00A0')) || text.equals("") || text.equals("¥n")) { //"",&nbsp;改行のみ
+                                            text = "";
+                                        } else {
+                                            String style = tdData.get(i == 0 ? pn + 2 : pn + 1).select("span").attr("style");
+                                            int m = style.indexOf("#");
+                                            if (m >= 0) color = style.substring(m, m + 7);
+                                        }
+                                        //DBへの登録
+                                        String[] column = {"_id","building_id","place","weekday","time","cell_text","cell_color"};
+                                        String[] value = {"", String.valueOf(building_id++),places[pn],String.valueOf(d),String.valueOf(i),text,color};
+                                        dbAdapter.saveDB("ClassroomDivide", column, value);
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+
+
+                } catch (Exception e) {
+                    new AlertDialog.Builder(parentActivity)
+                            .setTitle(R.string.error_title_data_response)
+                            .setMessage(R.string.error_message_data_invalid)
+                            .setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //okボタンが押された時の処理
+                                    parentActivity.finish();
+                                }
+                            })
+                            .show();
+                }finally {
+                    //DBを閉じる
+                    dbAdapter.closeDB();
+                }
+            }
+
+        }
     }
 }
